78904c631eaceba9a528ebe5f74f393e125b033f
Rumit Tandukar <rumit0270@gmail.com>
2019-06-16 10:49:26 +0545

    add morgan for logging and use dotenv for environment variables

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -12,10 +14,10 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.POSTGRES_HOST,
+    user : process.env.POSTGRES_USER,
+    password : process.env.POSTGRES_PASSWORD,
+    database : process.env.POSTGRES_DB
   }
 });
 
@@ -23,6 +25,7 @@ const app = express();
 
 app.use(cors())
 app.use(bodyParser.json());
+app.use(morgan('combined'));
 
 app.get('/', (req, res)=> { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
